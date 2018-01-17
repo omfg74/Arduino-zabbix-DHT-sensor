2b2dd8c1f3d7fcc44c3156553b7aed27d0e1992d
Vitor Frota <vitord2@hotmail.com>
2018-01-17 17:10:03 +0000

    Versão 2.0

--- TemperaturaArduino.ino
+++ TemperaturaArduino.ino
@@ -1,115 +1,58 @@
+#include <UIPEthernet.h>
 #include <DHT.h>
-#include <SPI.h>
-#include <Ethernet.h>
 
-#define DHTPIN 2     
-#define DHTTYPE DHT11
-#define MAX_CMD_LENGTH 25
+#define MACADDRESS 0xDE, 0xDE, 0xBE, 0xEF, 0xFE, 0xEE 
+#define IP 192,168,0,250
+#define LISTENPORT 10050
 
-DHT dht(DHTPIN, DHTTYPE);
-      
-//Rede
-byte mac[] = { 0xDE, 0xDE, 0xBE, 0xEF, 0xFE, 0xED }; //Define o mac do arduino
-IPAddress ip(192,168,0,251); //define o ip do arduino caso o dhcp no funcione
-      
-EthernetClient cliente;
-EthernetServer servidor = EthernetServer(10050); //Zabbix Agent port
-      
-String cmd; //Recebe o comando do zabbix
-boolean conectado=false;
-int contador=0;
-int portaPresenca=3;
-      
+#define DHTPIN 2
+#define DHTYPE DHT11
+
+
+EthernetServer server = EthernetServer(LISTENPORT);
+
+DHT dht(DHTPIN,DHTYPE);
+
+String msg ="";
 void setup() {
-  //iniciaSerial();
-  iniciaRede();
-       
+  //NETWORK
+  uint8_t mac[6] = {MACADDRESS};
+  uint8_t ip[4] = {IP};
+
+  Ethernet.begin(mac,ip);
+  server.begin();
+
+  //DHT Sensor
   dht.begin();
+  
+}
+
+void loop() {
+  EthernetClient client = server.available();
+  if (client) {
+    if (client.available() > 0) {
+      char thisChar = client.read();
+      if (thisChar == '\n') {
+
+        if(msg.equals("agent.ping") > 0) {
+          server.println("1");           
+        }
+         if(msg.equals("temp") > 0) {
+          server.println(dht.readTemperature());           
+        }
+         if(msg.equals("umid") > 0) {
+          server.println(dht.readHumidity());           
+        }
+         if(msg.equals("version") > 0) {
+          server.println("2.0");           
+        }
         
-  servidor.begin();
-	
-  pinMode(portaPresenca,INPUT);
- }
-      
-void loop() {    
-  cliente = servidor.available();
-  if(cliente == true){
-    if(!conectado){
-      cliente.flush();
-      conectado=true;
-    }
-    if(cliente.available()>0){
-      recebeComando(cliente.read());
+        client.stop();
+        
+        msg="";
+      }else {
+        msg += thisChar;  
+      }
     }
   }
-  //imprimirTemperatura();
-}
-      
-void iniciaSerial(){
-  Serial.begin(9600);
-  while(!Serial) { ; }
-  Serial.println("Porta Serial Iniciada");  
-}
-      
-void iniciaRede(){
-  if (Ethernet.begin(mac) == 0) {
-    //Serial.println("Falha ao pegar ip na rede");
-    //Serial.println("Iniciando Configuracao manual da rede");
-    Ethernet.begin(mac, ip);
-  } 
-  delay(1000);
-  //Serial.println("Rede Iniciada");
-}
-      
-void recebeComando(char c){
-  if(cmd.length() == MAX_CMD_LENGTH) {
-    cmd = "";
-  }
-  cmd += c;
-  if(c == '\n') {
-    if(cmd.length() > 2) {
-      // remove \r and \n from the string
-      cmd = cmd.substring(0,cmd.length() - 1);
-      tratarComando();
-    }
-  }
-}
-      
-void tratarComando(){
-  if(cmd.equals("agent.ping")){
-    servidor.println("1");
-    //Serial.println("Ping: 1"); 
-    cliente.stop();
-  }       
-  else if(cmd.equals("version")){
-    servidor.println("1.1");
-    cliente.stop();
-  } 
-  else if(cmd.equals("temp")){
-    float t = dht.readTemperature();
-    servidor.println(t);
-    //Serial.print("Temperatura: ");  
-    //Serial.println(t);
-    cliente.stop();
-    delay(2000);
-  } 
-  else if(cmd.equals("umid")){
-    float h = dht.readHumidity();
-    servidor.println(h);
-    //Serial.print("Umidade: ");  
-    //Serial.println(h);
-    cliente.stop();
-    delay(2000);
-  }
-  else if(cmd.equals("pres")){
-    servidor.println(digitalRead(portaPresenca));
-    cliente.stop();
-  }
-  else {
-    servidor.println("ZBX_NOTSUPPORTED");
-    cliente.stop();
-   //Serial.println("ZBX_NOTSUPPORTED");
-   //Serial.println(cmd);
-  }
-  cmd="";
 }
